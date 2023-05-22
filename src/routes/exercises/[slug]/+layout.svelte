6bd0325789af359860103e9de75139c31c1da613
Jesús Enrique Rascón <blograso@gmail.com>
2023-05-22 17:10:14 +0200

    transition

--- src/routes/exercises/[slug]/+layout.svelte
+++ src/routes/exercises/[slug]/+layout.svelte
@@ -1,10 +1,15 @@
-<script>
+<script lang="ts">
 	import ContinueButton from '$lib/Components/ui/ContinueButton.svelte';
 	import ExerciseDetails from '$lib/Components/ui/ExerciseDetails.svelte';
 	import ExerciseTitle from '$lib/Components/ui/ExerciseTitle.svelte';
 	import FinishButton from '$lib/Components/ui/FinishButton.svelte';
 	import PreviousButton from '$lib/Components/ui/PreviousButton.svelte';
 	import Steps from '$lib/Components/ui/Steps.svelte';
+
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
+
 	export let data;
 
 	const totalQuestions = data.total;
@@ -12,6 +17,9 @@
 	const exerciseNumber = data.number;
 	const nextExercise = (exerciseNumber + 1).toString();
 	const previousExercise = (exerciseNumber - 1).toString();
+
+	let visible: boolean = false;
+	onMount(() => (visible = true));
 </script>
 
 <svelte:head>
@@ -20,25 +28,29 @@
 
 <Steps current={exerciseNumber} total={totalQuestions} />
 
-<ExerciseTitle number={exerciseNumber.toString()} />
-<ExerciseDetails bloque={data.question.bloque} tema={data.question.tema} />
+{#if visible}
+	<div in:fly={{ y: 20, duration: 300, easing: cubicOut }}>
+		<ExerciseTitle number={exerciseNumber.toString()} />
+		<ExerciseDetails bloque={data.question.bloque} tema={data.question.tema} />
 
-<slot />
+		<slot />
 
-<div class="flex mt-20 justify-between">
-	{#if data.number > 1}
-		<a target="_self" href={`/exercises/${previousExercise}`}>
-			<PreviousButton />
-		</a>
-	{/if}
-	{#if data.number < totalQuestions}
-		<a target="_self" href={`/exercises/${nextExercise}`}>
-			<ContinueButton />
-		</a>
-	{/if}
-	{#if data.number == totalQuestions}
-		<a target="_self" href={`/exercises/results`}>
-			<FinishButton />
-		</a>
-	{/if}
-</div>
+		<div class="flex mt-20 justify-between">
+			{#if data.number > 1}
+				<a target="_self" href={`/exercises/${previousExercise}`}>
+					<PreviousButton />
+				</a>
+			{/if}
+			{#if data.number < totalQuestions}
+				<a target="_self" href={`/exercises/${nextExercise}`}>
+					<ContinueButton />
+				</a>
+			{/if}
+			{#if data.number == totalQuestions}
+				<a target="_self" href={`/exercises/results`}>
+					<FinishButton />
+				</a>
+			{/if}
+		</div>
+	</div>
+{/if}
--- src/routes/exercises/[slug]/+page.svelte
+++ src/routes/exercises/[slug]/+page.svelte
@@ -2,16 +2,24 @@
 	import QuestionT1 from '$lib/Components/ui/subtipo1/QuestionT1.svelte';
 	import QuestionT2 from '$lib/Components/ui/subtipo2/QuestionT2.svelte';
 	import QuestionT3 from '$lib/Components/ui/subtipo3/QuestionT3.svelte';
+	import { onMount } from 'svelte';
 	import { respuestas } from '../../../stores.js';
+	import { fly } from 'svelte/transition';
 
 	export let data;
 	const type = data.question._type;
+	let visible;
+	onMount(() => (visible = true));
 </script>
 
-{#if type === 'subtipo1'}
-	<QuestionT1 {data} />
-{:else if type === 'subtipo2'}
-	<QuestionT2 question={data.question} />
-{:else if type === 'subtipo3'}
-	<QuestionT3 {data} />
+{#if visible}
+	<div in:fly={{ y: 20, duration: 300 }}>
+		{#if type === 'subtipo1'}
+			<QuestionT1 {data} />
+		{:else if type === 'subtipo2'}
+			<QuestionT2 question={data.question} />
+		{:else if type === 'subtipo3'}
+			<QuestionT3 {data} />
+		{/if}
+	</div>
 {/if}
